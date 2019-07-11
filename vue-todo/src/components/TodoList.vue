c8922c33a7b565c3b20c87a0825d750faee1f865
gumju park <parkgumju@gmail.com>
2019-07-11 09:34:42 +0900

    브런치 생성

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -5,8 +5,6 @@
                 <i class="checkBtn fas fa-check"  v-bind:class="{checkBtnCompleted : todoItem.completed }" v-on:click="toggleComplete(todoItem, index)"></i>
                 <span v-bind:class="{textCompleted : todoItem.completed }">{{ todoItem.item }}</span>
                 <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
-
-
                     <i class="far fa-trash-alt"></i>
                 </span>
             </li>
